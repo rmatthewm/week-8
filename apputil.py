a6f1008b1263079da4faf5e3e1043dfb3a997436
Raul Matthew Mosley <raulmatthewmosley@gmail.com>
2026-03-11 17:39:26 -0400

    began work on building dictionary

--- apputil.py
+++ apputil.py
@@ -6,16 +6,15 @@ from collections import defaultdict
 
 class MarkovText(object):
 
-    def __init__(self, corpus, pre_cleaned=False):
+    def __init__(self, corpus, pre_cleaned=False, include_repeats=True):
         # Clean the corpus data given if needed
         if pre_cleaned:
             self.corpus = corpus
         else:
             self.corpus = self.clean_data(corpus)
 
-        print(self.corpus[:2000])
-
-        self.term_dict = None  # you'll need to build this
+        # Get the term dictionary that we will use for generation
+        self.term_dict = self.get_term_dict(include_repeats)
 
     def clean_data(self, corpus):
         # Replace the new lines with spaces
@@ -39,13 +38,25 @@ class MarkovText(object):
         # Return the cleaned data
         return corpus
 
-    def get_term_dict(self):
+    def get_term_dict(self, include_repeats=True):
+        # We will use a defaultdict so that every key
+        # will be initialized to an empty list. Then we
+        # can just append to a given key's list without
+        # creating that key first
+        term_dict = defaultdict(list)
 
-        # your code here ...
+        # Split the corpus into words
+        words = self.corpus.split(' ')
 
-        self.term_dict = {}
+        # Now we can check every word and add the following word
+        # to the dictionary depending on the repeats setting
+        for i in range(len(words) - 1):
+            if include_repeats or not words[i+1] in term_dict[words[i]]:
+                term_dict[words[i]].append(words[i+1])
 
-        return None
+        print(term_dict)
+
+        return term_dict 
 
 
     def generate(self, seed_term=None, term_count=15):
